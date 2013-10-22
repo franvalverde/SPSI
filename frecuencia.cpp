f98c47a62fce3cbcb1995cfd0547912d82bcf56a
rafaelgonz <rafa_1_9_9_2@hotmail.com>
2013-10-22 17:44:13 +0200

    parte rafa

---
+++ frecuencia.cpp
@@ -0,0 +1,14 @@
+//  Created by Rafa González on 22/10/13.
+//  Copyright (c) 2013 RGJ. All rights reserved.
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+	
+	int aparicion_letra;
+	int num_letras;
+	int frecuencia;
+	
+	frecuencia = aparicion_letra/num_letras;
